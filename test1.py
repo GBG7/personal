6c244b2ecfb3adb46995154529a185ffdc91e7a6
GBG7 <quantumaraa@gmail.com>
2022-11-04 13:14:24 -0400

    fixing imgur link + a few chgs

---
+++ test1.py
@@ -0,0 +1,9 @@
+listA = ['', '', '', '', 'Perez', '', '', '', 'Guerrero']
+listB = []
+for item in listA:
+    if item != "":
+        listB.append(item)
+print(listA)
+        
+listA = listB
+print (listA)
